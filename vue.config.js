590ed5c15796f3c3e60a8581ddacc8f4704a9cc4
antizey <zvgtrs@163.com>
2025-11-27 09:37:18 +0800

    修改publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/socialmediasimulator/',
   devServer: {
     proxy: {
       '/llmapi': {

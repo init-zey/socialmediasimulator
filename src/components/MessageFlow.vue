7d9d2665de75fbc4dbe013e10eb7db7542ade82c
antizey <zvgtrs@163.com>
2025-11-28 15:32:56 +0800

    MessageFlow.vue

---
+++ src/components/MessageFlow.vue
@@ -0,0 +1,38 @@
+<template>
+    <n-card class="flow">
+        <center>
+            <n-tag v-if="flow==0">主推送流</n-tag>
+            <n-input v-else v-model:value="label" size="small" type="text" placeholder="自定义推送流"/>
+        </center>
+        <draggable
+            v-model="source"
+            animation="200"
+            item-key="element"
+            group="flow"
+            style="height: 100%;"
+        >
+        <template #item="element">
+            <div>
+                <MessageCard :msg="progress.messages[element.element]">
+                    <template #action>
+                        <n-button @click="source=source?.filter(m=>m!=element.element)">移除</n-button>
+                    </template>
+                </MessageCard>
+            </div>
+        </template>
+        <!-- <p>{{ element }}</p> -->
+        </draggable>
+    </n-card>
+</template>
+
+<script setup lang="ts">
+import { progress } from '@/game';
+import MessageCard from './MessageCard.vue';
+import draggable from 'vuedraggable';
+import { defineProps, defineModel } from 'vue';
+import { NButton, NInput, NTag, NCard } from 'naive-ui';
+
+defineProps<{flow:number}>();
+const source = defineModel<Array<number>>("source");
+const label = defineModel<string>("label");
+</script>
